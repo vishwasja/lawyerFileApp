7536aa08dc3719b7320e47ac188f311cbf785329
Vishwas Jain <vishwas@bitbakery.io>
2016-09-22 21:29:01 +0530

    Initial Commit

---
+++ LawyerFileApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  LawyerFileApp
+//
+//  Created by bit2 on 22/09/16.
+//
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
